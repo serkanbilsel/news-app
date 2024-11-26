7c514aa5d18dea5cd3d62038ef9690305f9215bf
Serkan Bilsel <126615917+serkanbilsel@users.noreply.github.com>
2024-11-26 17:43:17 +0300

    Updates

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.bnews.app',
-  appName: 'Bnews',
-  webDir: 'build'
-};
-
-export default config;
